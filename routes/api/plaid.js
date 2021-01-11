405751e1ec6fdafab310f531995375f7a17ab555
happyjang <74928257+happyjang@users.noreply.github.com>
2021-01-11 15:28:48 -0800

    make it better

---
+++ routes/api/plaid.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const { addAccounts, getAccounts, getTransactions, deleteAccounts } = require("../../controllers/plaid-controller");
+
+// Routes will go here
+router.post(
+    "/accounts/add",
+    passport.authenticate("jwt", { session: false }),
+    addAccounts
+);
+router.get(
+    "/accounts",
+    passport.authenticate("jwt", { session: false }),
+    getAccounts
+  );
+
+  router.delete(
+    "/accounts/:id",
+    passport.authenticate("jwt", { session: false }),
+    deleteAccounts
+  );
+
+  router.post(
+    "/accounts/transactions",
+    passport.authenticate("jwt", { session: false }),
+    getTransactions
+  );
+  
+
+module.exports = router;
